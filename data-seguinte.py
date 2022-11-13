4b137708f819ce4da90ac6c0a355e46f6e2fb94f
Giovana da Silveira <giovana-2001@hotmail.com>
2022-11-13 17:09:49 -0300

    first commit

---
+++ data-seguinte.py
@@ -0,0 +1,5 @@
+from datetime import datetime, timedelta
+
+data_atual = datetime.now()
+data_futura = data_atual + timedelta(2)
+print(f"\nPassando dois dias a partir de hoje será {data_futura}\n")
