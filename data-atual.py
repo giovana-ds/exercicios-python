89cd142fa5668b63293b76c8e279b0fc45ff44eb
Giovana da Silveira <giovana-2001@hotmail.com>
2022-11-13 18:07:04 -0300

    exercícios de data

---
+++ data-atual.py
@@ -0,0 +1,3 @@
+from datetime import datetime
+data_atual = datetime.now()
+print(f"\nHoje é {data_atual.date()}.")
--- data-seguinte.py
+++ data-seguinte.py
@@ -2,4 +2,4 @@ from datetime import datetime, timedelta
 
 data_atual = datetime.now()
 data_futura = data_atual + timedelta(2)
-print(f"\nPassando dois dias a partir de hoje será {data_futura}\n")
+print(f"\nPassando dois dias a partir de hoje será {data_futura}.\n")
