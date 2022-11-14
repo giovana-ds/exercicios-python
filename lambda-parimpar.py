2fd07580eac189cd1f2de02909ebcff91e9b76c7
Giovana da Silveira <102772845+giovana-ds@users.noreply.github.com>
2022-11-14 19:16:43 -0300

    Delete lambda-parimpar.py

--- lambda-parimpar.py
+++
@@ -1,7 +0,0 @@
-numero = int(input("\nInforme um número qualquer: "))
-par = lambda numero: numero % 2 == 0
-
-if par(numero):
-    print(f"O número {numero} é par.")
-else:
-    print(f"O número {numero} é ímpar.")
