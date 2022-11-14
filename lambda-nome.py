2448e85f62c0aef3234dd09e463579ffab587877
Giovana da Silveira <102772845+giovana-ds@users.noreply.github.com>
2022-11-14 19:16:36 -0300

    Delete lambda-nome.py

--- lambda-nome.py
+++
@@ -1,5 +0,0 @@
-nomecompleto = str(input("\nInforme o seu nome completo: "))
-nome = lambda nome: nome.split()[0]
-sobrenome = lambda nome: nome.split()[-1]
-
-print(f"\nSeu primeiro nome é {nome(nomecompleto)} e o seu sobrenome é {sobrenome(nomecompleto)}.")
